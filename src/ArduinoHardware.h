161d680a0a1e81defdd0a7a5b6093d9c48e6da32
naderkamali <69947750+naderkamali@users.noreply.github.com>
2023-03-14 03:15:34 -0700

    Update ArduinoHardware.h
    Switched to serial1.

--- src/ArduinoHardware.h
+++ src/ArduinoHardware.h
@@ -77,7 +77,7 @@ class ArduinoHardware {
 #elif defined(USE_TEENSY_HW_SERIAL) or defined(USE_STM32_HW_SERIAL)
       iostream = &Serial1;
 #else
-      iostream = &Serial;
+      iostream = &Serial1;
 #endif
       baud_ = 57600;
     }
